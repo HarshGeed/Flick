15e8021964b471e0704aa5deee48a792d600b223
HarshGeed <harshgeed07@gmail.com>
2025-05-18 12:21:01 +0530

    backend for fetching movie details completed

---
+++ app/(backend)/api/movies_section/movie_details/[movie_id]/recommendations/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { movie_id: string } }
+) {
+  const { movie_id } = params;
+  if (!movie_id) {
+    return NextResponse.json({ error: "Movie ID is required" }, { status: 400 });
+  }
+
+  const url = `https://api.themoviedb.org/3/movie/${movie_id}/recommendations?language=en-US&page=1`;
+
+  try {
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.TMDB_API_TOKEN}`,
+      },
+      next: { revalidate: 3600 },
+    });
+
+    if (!res.ok) {
+      return NextResponse.json(
+        { error: "Failed to fetch movie details" },
+        { status: res.status }
+      );
+    }
+
+    const data = await res.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
